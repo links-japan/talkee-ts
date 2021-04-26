f9dc4b87022b76e436dc9fa96530c7e8a7fb5c0e
Lyric <5h3ll3x@gmail.com>
2021-04-26 18:28:48 +0900

    fix: _

--- src/views/comments.ts
+++ src/views/comments.ts
@@ -84,7 +84,7 @@ export default class Comments {
     // fetch the comment, and append it to the top
     const comment: any = await apis.getComment(commentId);
     this.spotlight?.append(this.talkee.buildCommentUI(comment, null));
-    this.element?.scrollIntoView();
+    this.talkee.commentsContainer?.scrollIntoView();
   }
 
   prepend(comments) {
